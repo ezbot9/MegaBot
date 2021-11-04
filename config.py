8215a1c1e9cb2d4a23c00688b9472b817dae7a22
Ezboyy <91524614+ezbot9@users.noreply.github.com>
2021-11-04 14:28:48 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2021 Itz-fork
+
+import os
+
+class Config(object):
+    APP_ID = int(os.environ.get("APP_ID", 1234567))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    DOWNLOAD_LOCATION = "./EzBots"
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "False")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", -1234567))
+    TG_MAX_SIZE = 2040108421
+    # Mega User Account
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "")
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "")
+
+# Text Prints
+B_START_TEXT = """
+   __  ___                                ___       __ 
+  /  |/  /__ ___ ____ _      ___  ___    / _ )___  / /_
+ / /|_/ / -_) _ `/ _ `/ _   / _ \/_ /   / _  / _ \/ __/
+/_/  /_/\__/\_, /\_,_/ (_) /_//_//__/  /____/\___/\__/ 
+           /___/
+
+Process: {}
+"""
+
+PROCESS_TEXT = """
+Process: {}
+"""
+
+LOGGED_AS_USER = """
+Successfully Logged Into Mega.nz User Account
+"""
+
+LOGIN_ERROR_TEXT = """
+Can't Get Mega Email and Password Login as an Anonymouse User
+"""
+
+ERROR_TEXT = """
+ _____                     
+| ____|_ __ _ __ ___  _ __
+|  _| | '__| '__/ _ \| '__|
+| |___| |  | | | (_) | |   
+|_____|_|  |_|  \___/|_|  
+
+Log: {}
+
+Save the Log file and Send it to @Nexa_bots for Help :)
+"""
+
+START_TEXT="""
+    _   __                   ____        __      
+   / | / /__  _  ______ _   / __ )____  / /______
+  /  |/ / _ \| |/_/ __ `/  / __  / __ \/ __/ ___/
+ / /|  /  __/>  </ /_/ /  / /_/ / /_/ / /_(__  ) 
+/_/ |_/\___/_/|_|\__,_/  /_____/\____/\__/____/ 
+
+
+Bot is Running Now! Join @NexaBotsUpdates
+"""
